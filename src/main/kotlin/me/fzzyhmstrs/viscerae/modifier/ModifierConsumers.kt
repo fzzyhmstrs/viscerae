188aeb3aa34d6b93f03e0e7ec2315dd00ee61be0
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-01-09 15:02:41 -0500

    Update ModifierConsumers.kt

--- src/main/kotlin/me/fzzyhmstrs/viscerae/modifier/ModifierConsumers.kt
+++ src/main/kotlin/me/fzzyhmstrs/viscerae/modifier/ModifierConsumers.kt
@@ -14,11 +14,112 @@ import net.minecraft.item.ItemStack
 object ModifierConsumers {
 
     //weapon modifier consumers
-    val BLOOD_SLASH_CONSUMER: WeaponModifier.WeaponConsumer =
+    val BLOOD_SLASH_USE_CONSUMER: WeaponModifier.WeaponConsumer =
         WeaponModifier.WeaponConsumer {stack: ItemStack, user: LivingEntity, target: LivingEntity? ->
             val rot = user.getRotationVec(1.0f)
     }
+        
+    val BLOODTHIRSTY_USE_CONSUMER: WeaponModifier.WeaponConsumer =
+        WeaponModifier.WeaponConsumer {stack: ItemStack, user: LivingEntity, target: LivingEntity? ->
+            user.damage(DamageSource.STARVE, 10)
+            EffectQueue.addStatusToQueue(user,RegisterStatus.BLOODTHIRST,160,0)
+            EffectQueue.addStatusToQueue(user,StatusEffects.RESISTANCE,160,0)
+            if (user is PlayerEntity){
+                user.itemCooldownManager.set(stack.item,1200)
+            }
+    }
+        
+    val BLOODTHIRSTY_HIT_CONSUMER: WeaponModifier.WeaponConsumer =
+        WeaponModifier.WeaponConsumer {stack: ItemStack, user: LivingEntity, target: LivingEntity? ->
+            if (user.hasStatusEffect(RegisterStatus.BLOODTHIRST){
+                if (target != null && !target.isAlive()){
+                    val effect = user.getStatusEffect(RegisterStatus.BLOODTHIRST)
+                    val amp = effect?.amplifier?:0
+                    val duration = effect?.duration?:0
+                    if (duration > 0){
+                        val duration2 = if(duration < 160) {160} else {duration}
+                        user.addStatusEffect(StatusEffectInstance(RegisterStatus.BLOODTHIRST,duration2,amp + 1))
+                    }
+                }
+            }
+    }
 
+    val DEADBLOW_USE_CONSUMER: WeaponModifier.WeaponConsumer =
+        WeaponModifier.WeaponConsumer {stack: ItemStack, user: LivingEntity, target: LivingEntity? ->
+            if (target != null){
+                target.damage(DamageSource.MAGIC,10)
+                target.takeKnockback(2.0,user.x - target.x,user.z - target.z)
+                EffectQueue.addStatusToQueue(user,StatusEffects.SLOWNESS,100,3)
+                if (user is PlayerEntity){
+                    user.itemCooldownManager.set(stack.item,200)
+                }
+            }
+    }
+        
+    val FRENZIED_HIT_CONSUMER: WeaponModifier.WeaponConsumer =
+        WeaponModifier.WeaponConsumer {stack: ItemStack, user: LivingEntity, target: LivingEntity? ->
+            if (target != null && !target.isAlive()){
+                val effect = user.getStatusEffect(RegisterStatus.BLOODLUST)
+                val amp = effect?.amplifier?:0
+                val duration = effect?.duration?:0
+                if (duration > 0){
+                    val duration2 = if(duration < 80) {80} else {duration}
+                    user.addStatusEffect(StatusEffectInstance(RegisterStatus.BLOODLUST,duration2,amp + 1))
+                }
+            }
+    }
+        
+    val GLUTTONOUS_TICK_CONSUMER: WeaponModifier.WeaponConsumer =
+        WeaponModifier.WeaponConsumer {stack: ItemStack, user: LivingEntity, target: LivingEntity? ->
+            if (EventRegistry.ticker_40.isReady()){
+                if (stack.isDamaged && user is PlayerEntity){
+                    val nbt = stack.nbt
+                    if (stack.damage > 100 || (nbt != null && nbt.containsKey("feeding") && nbt.getBoolean("feeding") == true)){
+                        
+                    }
+                }
+            }
+    }
+        
+    val INNER_FIRE_HIT_CONSUMER: WeaponModifier.WeaponConsumer =
+        WeaponModifier.WeaponConsumer {stack: ItemStack, user: LivingEntity, target: LivingEntity? ->
+            if (target != null){
+                target.addStatusEffect(StatusEffectInstance(RegisterStatus.BLOOD_BOIL,100,1))
+            }
+    }
+        
+    val SOUL_BOMB_HIT_CONSUMER: WeaponModifier.WeaponConsumer =
+        WeaponModifier.WeaponConsumer {stack: ItemStack, user: LivingEntity, target: LivingEntity? ->
+            if (target != null && !target.isAlive()){
+                
+            }
+    }
+        
+    val SOUL_CHAINS_USE_CONSUMER: WeaponModifier.WeaponConsumer =
+        WeaponModifier.WeaponConsumer {stack: ItemStack, user: LivingEntity, target: LivingEntity? ->
+            if (target != null){
+                val entityList = RaycasterUtil.raycastEntityArea(4.5, user, target.pos)
+            }
+    }
+        
+    val VAMPIRIC_HIT_CONSUMER: WeaponModifier.WeaponConsumer =
+        WeaponModifier.WeaponConsumer {stack: ItemStack, user: LivingEntity, target: LivingEntity? ->
+            if (target != null){
+                val recentDamage = target.damageTracker.mostRecentDamage
+                val recentAmount = recentDamage?.damage?:0f
+                val healAmount = recentAmount * VisceraeConfig.items.vampiricSwordHealFraction
+                user.heal(healAmount)
+            }
+    }
+        
+    val VITAL_HIT_CONSUMER: WeaponModifier.WeaponConsumer =
+        WeaponModifier.WeaponConsumer {stack: ItemStack, user: LivingEntity, target: LivingEntity? ->
+            if (target != null && !target.isAlive()){
+                
+            }
+    }
+
+        
     //scepter modifier consumers
 
     val BLOOD_PACT_CONSUMER = AugmentConsumer({ list: List<LivingEntity> -> bloodPactConsumer(list) }, AugmentConsumer.Type.BENEFICIAL)
