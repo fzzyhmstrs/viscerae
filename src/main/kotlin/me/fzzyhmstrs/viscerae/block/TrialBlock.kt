28e6d0a8b99a66c8751a2e35fb36288fdc338f33
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-01-10 13:39:40 -0500

    Update TrialBlock.kt

--- src/main/kotlin/me/fzzyhmstrs/viscerae/block/TrialBlock.kt
+++ src/main/kotlin/me/fzzyhmstrs/viscerae/block/TrialBlock.kt
@@ -2,8 +2,33 @@ package me.fzzyhmstrs.viscerae.block
 
 import net.minecraft.block.*
 
-class TrialBlock(settings: Settings): Block(settings){
+class TrialBlock(settings: Settings): BlockWithEntity(settings){
 
+    @Deprecated("Deprecated in Java")
+    override fun getRenderType(state: BlockState): BlockRenderType {
+        return BlockRenderType.MODEL
+    }
     
+     override fun createBlockEntity(pos: BlockPos, state: BlockState): BlockEntity {
+        return TODO()
+    }
+     
+     override fun <T : BlockEntity> getTicker(
+        world: World,
+        state: BlockState,
+        type: BlockEntityType<T>
+    ): BlockEntityTicker<T>? {
+        return if(!world.isClient) checkType(
+            type, RegisterEntity.TRIAL_BLOCK_ENTITY
+        ) { world2: World, pos: BlockPos, state2: BlockState, blockEntity: TrialBlockEntity ->
+            TrialBlockEntity.tick(
+                world2,
+                pos,
+                state2,
+                blockEntity
+            )
+        } else null
+    }
+
 
 }
