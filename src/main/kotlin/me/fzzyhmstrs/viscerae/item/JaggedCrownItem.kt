51d22f5873ff55c3bd087b671eb1b25ff254d188
fzzyhmstrs <fzzyhmstrs@gmail.com>
2023-01-11 22:36:48 -0500

    start work on trinkets

--- src/main/kotlin/me/fzzyhmstrs/viscerae/item/JaggedCrownItem.kt
+++ src/main/kotlin/me/fzzyhmstrs/viscerae/item/JaggedCrownItem.kt
@@ -4,10 +4,15 @@ import me.fzzyhmstrs.amethyst_core.item_util.AbstractAugmentJewelryItem
 import me.fzzyhmstrs.amethyst_core.mana_util.ManaItem
 import me.fzzyhmstrs.amethyst_core.trinket_util.TrinketUtil
 import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.damage.DamageSource
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.ItemStack
 
 class JaggedCrownItem(settings: Settings): AbstractAugmentJewelryItem(settings) {
 
+    override fun postHit(stack: ItemStack, target: LivingEntity, attacker: LivingEntity): Boolean {
+        attacker.damage(DamageSource.thorns(target),1.0f)
+        return super.postHit(stack, target, attacker)
+    }
 
 }
