ea1fc487ee954631b29b9d829b75607e68859413
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-01-19 13:53:21 -0500

    Update RegisterModifier.kt

--- src/main/kotlin/me/fzzyhmstrs/viscerae/registry/RegisterModifier.kt
+++ src/main/kotlin/me/fzzyhmstrs/viscerae/registry/RegisterModifier.kt
@@ -16,30 +16,30 @@ object RegisterModifier {
     private val regMod: MutableList<AbstractModifier<*>> = mutableListOf()
 
     //weapon modifiers
-    val BLOOD_SLASH = EquipmentModifier(Identifier(Viscerae.MOD_ID,"blood_slash"))
+    val BLOOD_SLASH = EquipmentModifier(Identifier(Viscerae.MOD_ID,"blood_slash"), persistent = true, randomSelectable = false)
         .withOnUse(ModifierConsumers.BLOOD_SLASH_USE_CONSUMER)
         .withPostHit(ModifierConsumers.BLOOD_SLASH_HIT_CONSUMER).also { regMod.add(it) }
-    val BLOODTHIRSTY = EquipmentModifier(Identifier(Viscerae.MOD_ID,"bloodthirsty"))
+    val BLOODTHIRSTY = EquipmentModifier(Identifier(Viscerae.MOD_ID,"bloodthirsty"), persistent = true, randomSelectable = false)
         .withAttributeModifier(EntityAttributes.GENERIC_ATTACK_DAMAGE,"083dcbc6-908f-11ed-a1eb-0242ac120002",1.0,EntityAttributeModifier.Operation.ADDITION)
         .withOnUse(ModifierConsumers.BLOODTHIRSTY_USE_CONSUMER)
         .withPostHit(ModifierConsumers.BLOODTHIRSTY_HIT_CONSUMER).also { regMod.add(it) }
-    val DEADBLOW = EquipmentModifier(Identifier(Viscerae.MOD_ID,"deadblow"))
+    val DEADBLOW = EquipmentModifier(Identifier(Viscerae.MOD_ID,"deadblow"), persistent = true, randomSelectable = false)
         .withAttributeModifier(EntityAttributes.GENERIC_ATTACK_KNOCKBACK,"gr59yl13-908f-11ed-a1eb-0242ac120002",0.5,EntityAttributeModifier.Operation.ADDITION)
         .withOnUse(ModifierConsumers.DEADBLOW_USE_CONSUMER).also { regMod.add(it) }
-    val FRENZIED = EquipmentModifier(Identifier(Viscerae.MOD_ID,"frenzied"))
+    val FRENZIED = EquipmentModifier(Identifier(Viscerae.MOD_ID,"frenzied"), persistent = true, randomSelectable = false)
         .withPostHit(ModifierConsumers.FRENZIED_HIT_CONSUMER).also { regMod.add(it) }
-    val GLUTTONOUS = EquipmentModifier(Identifier(Viscerae.MOD_ID,"gluttonous"))
+    val GLUTTONOUS = EquipmentModifier(Identifier(Viscerae.MOD_ID,"gluttonous"), persistent = true, randomSelectable = false)
         .withTick(ModifierConsumers.GLUTTONOUS_TICK_CONSUMER).also { regMod.add(it) }
-    val INNER_FIRE = EquipmentModifier(Identifier(Viscerae.MOD_ID,"inner_fire"))
+    val INNER_FIRE = EquipmentModifier(Identifier(Viscerae.MOD_ID,"inner_fire"), persistent = true, randomSelectable = false)
         .withAttributeModifier(EntityAttributes.GENERIC_ATTACK_SPEED,"14ae83tp-908f-11ed-a1eb-0242ac120002",1.0,EntityAttributeModifier.Operation.MULTIPLY_TOTAL)
         .withPostHit(ModifierConsumers.INNER_FIRE_HIT_CONSUMER).also { regMod.add(it) }
-    val SOUL_BOMB = EquipmentModifier(Identifier(Viscerae.MOD_ID,"soul_bomb"))
+    val SOUL_BOMB = EquipmentModifier(Identifier(Viscerae.MOD_ID,"soul_bomb"), persistent = true, randomSelectable = false)
         .withPostHit(ModifierConsumers.SOUL_BOMB_HIT_CONSUMER).also { regMod.add(it) }
-    val SOUL_CHAINS = EquipmentModifier(Identifier(Viscerae.MOD_ID,"soul_chains"))
+    val SOUL_CHAINS = EquipmentModifier(Identifier(Viscerae.MOD_ID,"soul_chains"), persistent = true, randomSelectable = false)
         .withOnUse(ModifierConsumers.SOUL_CHAINS_USE_CONSUMER).also { regMod.add(it) }
-    val VAMPIRIC = EquipmentModifier(Identifier(Viscerae.MOD_ID,"vampiric"))
+    val VAMPIRIC = EquipmentModifier(Identifier(Viscerae.MOD_ID,"vampiric"), persistent = true, randomSelectable = false)
         .withPostHit(ModifierConsumers.VAMPIRIC_HIT_CONSUMER).also { regMod.add(it) }
-    val VITAL = EquipmentModifier(Identifier(Viscerae.MOD_ID,"vital"))
+    val VITAL = EquipmentModifier(Identifier(Viscerae.MOD_ID,"vital"), persistent = true, randomSelectable = false)
         .withPostHit(ModifierConsumers.VITAL_HIT_CONSUMER).also { regMod.add(it) }
 
     //scepter modifiers
