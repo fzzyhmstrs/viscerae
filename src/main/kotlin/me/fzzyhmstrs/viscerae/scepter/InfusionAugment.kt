9d505fb64dccacb30ef57a5e571552c88cf59987
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-01-26 12:36:22 -0500

    Create InfusionAugment.kt

---
+++ src/main/kotlin/me/fzzyhmstrs/viscerae/scepter/InfusionAugment.kt
@@ -0,0 +1,57 @@
+package me.fzzyhmstrs.amethyst_imbuement.scepter
+
+import me.fzzyhmstrs.amethyst_core.modifier_util.AugmentConsumer
+import me.fzzyhmstrs.amethyst_core.modifier_util.AugmentEffect
+import me.fzzyhmstrs.amethyst_core.scepter_util.LoreTier
+import me.fzzyhmstrs.amethyst_core.scepter_util.SpellType
+import me.fzzyhmstrs.amethyst_core.scepter_util.augments.AugmentDatapoint
+import me.fzzyhmstrs.amethyst_core.scepter_util.augments.MinorSupportAugment
+import net.minecraft.entity.Entity
+import net.minecraft.entity.EquipmentSlot
+import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.passive.GolemEntity
+import net.minecraft.entity.passive.PassiveEntity
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.item.Items
+import net.minecraft.sound.SoundCategory
+import net.minecraft.world.World
+
+class InfusionAugment(tier: Int, maxLvl: Int, vararg slot: EquipmentSlot): MinorSupportAugment(tier, maxLvl, *slot){
+
+    override val baseEffect: AugmentEffect
+        get() = super.baseEffect.withDamage(0.8F,0.2F,0.0F)
+
+    override fun supportEffect(
+        world: World,
+        target: Entity?,
+        user: LivingEntity,
+        level: Int,
+        effects: AugmentEffect
+    ): Boolean {
+        if(target != null) {
+            if (target is PassiveEntity || target is GolemEntity || target is PlayerEntity) {
+                val healthCheck = (target as LivingEntity).health
+                target.heal(effects.damage(level))
+                if (target.health == healthCheck) return false
+                world.playSound(null, target.blockPos, soundEvent(), SoundCategory.PLAYERS, 1.0F, 1.0F)
+                effects.accept(target, AugmentConsumer.Type.BENEFICIAL)
+                return true
+            }
+        }
+        return if (user is PlayerEntity) {
+            val healthCheck = user.health
+                user.heal(effects.damage(level))
+                if (user.health == healthCheck) return false
+                world.playSound(null, user.blockPos, soundEvent(), SoundCategory.PLAYERS, 1.0F, 1.0F)
+                effects.accept(user,AugmentConsumer.Type.BENEFICIAL)
+                true
+            } else {
+                false
+            }
+
+    }
+
+    override fun augmentStat(imbueLevel: Int): AugmentDatapoint {
+        return AugmentDatapoint(SpellType.GRACE,13,4,1,imbueLevel,LoreTier.LOW_TIER, Items.GLISTERING_MELON_SLICE)
+    }
+}
